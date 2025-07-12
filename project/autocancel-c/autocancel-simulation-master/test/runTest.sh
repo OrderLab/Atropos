2d501bf168dd6bf7a9db10e0467607159dc24a59
catbeat <1664158108@qq.com>
2025-07-12 11:18:51 +0800

    [autocancel] add c language implementation of autocancel

--- project/autocancel-c/autocancel-simulation-master/test/runTest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo sysbench /usr/local/share/sysbench/oltp_read_only.lua --db-driver=mysql \
---mysql-user=root --mysql-password=adgjla \
---mysql-db=test --tables=3 --table-size=100000 --threads=2 --time=60 --report-interval=0.1 --percentile=50 run > ./output 2>&1 &
-
-sudo ./test.o
